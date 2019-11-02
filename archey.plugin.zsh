1c27ce80951ff309a672c888b3d38dbe5e9e7aa6
Alexandru Rosianu <me@aluxian.com>
2019-11-02 20:19:04 +0000

    rm use of arrays

--- archey.plugin.zsh
+++ archey.plugin.zsh
@@ -5,7 +5,7 @@ function archey() {
   kernel=$(uname)
   shell="$SHELL"
   terminal="$TERM ${TERM_PROGRAM//_/ }"
-  term_font="$(kitty --debug-config | awk '/^font_family|^font_size/ {$1="";gsub("^ *","",$0);print $0}')"
+  term_font="$(kitty --debug-config 2>/dev/null | awk '/^font_family|^font_size/ {$1="";gsub("^ *","",$0);print $0}')"
   location="$(networksetup -getcurrentlocation)"
   cpu=$(sysctl -n machdep.cpu.brand_string)
   gpu="$(system_profiler SPDisplaysDataType | awk -F': ' '/^\ *Chipset Model:/ {printf $2 ", "}' | rev | cut -c3- | rev)"
@@ -19,32 +19,20 @@ function archey() {
   textColor=$(tput setaf 6 2>/dev/null)
   normal=$(tput    sgr0 2>/dev/null)
 
-  fieldlist[${#fieldlist[@]}]="${textColor}User:${normal} ${user}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Hostname:${normal} ${hostname}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Distro:${normal} ${distro}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Kernel:${normal} ${kernel}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Shell:${normal} ${shell}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Terminal:${normal} ${terminal}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Font Family:${normal} ${term_font}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}Network Location:${normal} ${location}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}CPU:${normal} ${cpu}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}GPU:${normal} ${gpu}${normal}"
-  fieldlist[${#fieldlist[@]}]="${textColor}RAM:${normal} ${ram}${normal}"
-
   echo -e "
-  ${GREEN#  }                 ###               ${fieldlist[0]}
-  ${GREEN#  }               ####                ${fieldlist[1]}
-  ${GREEN#  }               ###                 ${fieldlist[2]}
-  ${GREEN#  }       #######    #######          ${fieldlist[3]}
-  ${YELLOW# }     ######################        ${fieldlist[4]}
-  ${YELLOW# }    #####################          ${fieldlist[5]}
-  ${RED#    }    ####################           ${fieldlist[6]}
-  ${RED#    }    ####################           ${fieldlist[7]}
-  ${RED#    }    #####################          ${fieldlist[8]}
-  ${PURPLE# }     ######################        ${fieldlist[9]}
-  ${PURPLE# }      ####################         ${fieldlist[10]}
-  ${BLUE#   }        ################           ${fieldlist[11]}
-  ${BLUE#   }         ####     #####            ${fieldlist[12]}
+  ${GREEN#  }                 ###               ${textColor}User:${normal} ${user}${normal}
+  ${GREEN#  }               ####                ${textColor}Hostname:${normal} ${hostname}${normal}
+  ${GREEN#  }               ###                 ${textColor}Distro:${normal} ${distro}${normal}
+  ${GREEN#  }       #######    #######          ${textColor}Kernel:${normal} ${kernel}${normal}
+  ${YELLOW# }     ######################        ${textColor}Shell:${normal} ${shell}${normal}
+  ${YELLOW# }    #####################          ${textColor}Terminal:${normal} ${terminal}${normal}
+  ${RED#    }    ####################           ${textColor}Font Family:${normal} ${term_font}${normal}
+  ${RED#    }    ####################           ${textColor}Network Location:${normal} ${location}${normal}
+  ${RED#    }    #####################          ${textColor}CPU:${normal} ${cpu}${normal}
+  ${PURPLE# }     ######################        ${textColor}GPU:${normal} ${gpu}${normal}
+  ${PURPLE# }      ####################         ${textColor}RAM:${normal} ${ram}${normal}
+  ${BLUE#   }        ################           
+  ${BLUE#   }         ####     #####            
   ${normal}
   "
 }
