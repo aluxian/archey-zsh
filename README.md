5e8d6e13afc00a1f976f9513b57df854ce5edbed
Alexandru Rosianu <me@aluxian.com>
2019-11-02 20:05:24 +0000

    convert to zsh plugin

--- README.md
+++ README.md
@@ -1,50 +1,11 @@
-# Archey for OS X
-An archey script for OS X.
+# Archey for OS X — ZSH Plugin
 
-```sh
-                 ###                  User: jhann
-               ####                   Hostname: obihann
-               ###                    Distro: OS X 10.10.5
-       #######    #######             Kernel: Darwin
-     ######################           Uptime:  6:11
-    #####################             Shell: /bin/zsh
-    ####################              Terminal: xterm-256color iTerm.app
-    ####################              Packages: 114
-    #####################             CPU: Intel Core i5-3210M CPU @ 2.50GHz
-     ######################           Memory: 8 GB
-      ####################            Disk: 95%
-        ################              Battery: 100.00%
-         ####     #####               IP Address: 24.222.18.26
-```
+Massively simplified clone of https://github.com/obihann/archey-osx optimised for speed.
 
-## Table Of Contents
-* [Download](#download)
-* [Installation](#installation)
-* [Options](#options)
-* [Credits](#credits)
-* [License](#license)
-
-## Download
-The latest stable release is [1.6.0](https://github.com/obihann/archey-osx/archive/1.6.0.tar.gz) (md5 82a064d8ce8069f1fa6e9bdf570ebca7).
-
-## Installation
-To get started you will need [homebrew](http://brew.sh/) to manage packages such as Python, figlet, and cowsay. To install please run the following command:
-
-```bash
-brew install archey
-```
-
-## Options
-* -b,  --nocolor : Use black & white logo
-* -c,  --color   : Force using a color Logo
-* -p   --packager  Use auto detected package system (default packager: homebrew)
-* -m,  --macports : Use MacPorts instead of Homebrew to display package count
-* -o   --offline : Disable the IP address check
-* -l   --localip : Show the local IP address associated with the default adapter
-* -h,  --help : Show help
-
+Install like any other zsh plugin.
 
 ## Credits
+
 * [djmelik](https://github.com/djmelik/archey) - Archey
 * [joshfinnie](https://github.com/joshfinnie/archey-osx) - A great OSX Python port of Archey
 * [Gary00](https://github.com/Gary00/archey-osx) - A fork of joshfinnie's Archey port, and the base of this script.
@@ -54,6 +15,7 @@ brew install archey
 * [vladshub](https://github.com/vladshub) - Custom logo support
 
 ## License
+
 This tool is protected by the [GNU General Public License v2](http://www.gnu.org/licenses/gpl-2.0.html).
 
-Copyright [Jeffrey Hann](http://jeffreyhann.ca/) 2016
+Copyright 2019
---
+++ archey.plugin.zsh
@@ -0,0 +1,55 @@
+function archey() {
+  user=$(whoami)
+  hostname=$(hostname | sed 's/.local//g')
+  distro="macOS $(sw_vers -productVersion)"
+  kernel=$(uname)
+  shell="$SHELL"
+  terminal="$TERM ${TERM_PROGRAM//_/ }"
+  term_font="$(kitty --debug-config | awk '/^font_family|^font_size/ {$1="";gsub("^ *","",$0);print $0}')"
+  location="$(networksetup -getcurrentlocation)"
+  cpu=$(sysctl -n machdep.cpu.brand_string)
+  gpu="$(system_profiler SPDisplaysDataType | awk -F': ' '/^\ *Chipset Model:/ {printf $2 ", "}' | rev | cut -c3- | rev)"
+  ram="$(( $(sysctl -n hw.memsize) / 1024 ** 3  )) GB"
+
+  RED=$(tput       setaf 1 2>/dev/null)
+  GREEN=$(tput     setaf 2 2>/dev/null)
+  YELLOW=$(tput    setaf 3 2>/dev/null)
+  BLUE=$(tput      setaf 4 2>/dev/null)
+  PURPLE=$(tput    setaf 5 2>/dev/null)
+  textColor=$(tput setaf 6 2>/dev/null)
+  normal=$(tput    sgr0 2>/dev/null)
+
+  fieldlist[${#fieldlist[@]}]="${textColor}User:${normal} ${user}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Hostname:${normal} ${hostname}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Distro:${normal} ${distro}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Kernel:${normal} ${kernel}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Shell:${normal} ${shell}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Terminal:${normal} ${terminal}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Font Family:${normal} ${term_font}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}Network Location:${normal} ${location}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}CPU:${normal} ${cpu}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}GPU:${normal} ${gpu}${normal}"
+  fieldlist[${#fieldlist[@]}]="${textColor}RAM:${normal} ${ram}${normal}"
+
+  echo -e "
+  ${GREEN#  }                 ###               ${fieldlist[0]}
+  ${GREEN#  }               ####                ${fieldlist[1]}
+  ${GREEN#  }               ###                 ${fieldlist[2]}
+  ${GREEN#  }       #######    #######          ${fieldlist[3]}
+  ${YELLOW# }     ######################        ${fieldlist[4]}
+  ${YELLOW# }    #####################          ${fieldlist[5]}
+  ${RED#    }    ####################           ${fieldlist[6]}
+  ${RED#    }    ####################           ${fieldlist[7]}
+  ${RED#    }    #####################          ${fieldlist[8]}
+  ${PURPLE# }     ######################        ${fieldlist[9]}
+  ${PURPLE# }      ####################         ${fieldlist[10]}
+  ${BLUE#   }        ################           ${fieldlist[11]}
+  ${BLUE#   }         ####     #####            ${fieldlist[12]}
+  ${normal}
+  "
+}
+
+function _cached_archey() {
+  local CACHE_FILE_PATH=/tmp/archey-cache.txt
+  [ -f $CACHE_FILE_PATH ] && cat $CACHE_FILE_PATH || archey | tee $CACHE_FILE_PATH
+}
